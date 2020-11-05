603646be8fdf8ec749e16d78be485f895e554f84
Matteo Lang <matteolang@air-de-maru.lan>
2020-11-05 16:24:10 -0300

    empeze charts de home con API

---
+++ js/index.js
@@ -0,0 +1,33 @@
+
+window.addEventListener(`load`, function(){
+
+var linkimagen = "https://image.tmdb.org/t/p/w500"
+
+fetch(`https://api.themoviedb.org/3/movie/popular?api_key=c5fa76b40f5a5ea03c60140eade37d35&language=en-US&page=1`)
+    .then(function(response){
+    return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+
+        var imagenescontenedor = document.querySelector(`#container-populares`)
+
+        for (let index = 0; index < data.results.length; index++) {
+            var results = data.results[index]
+            
+            console.log(`${linkimagen}${results.poster_path}`)
+
+
+            imagenescontenedor.innerHTML += 
+           `<article class="caja">
+            <a href="serieDetail.html" class="pelicula-serie"><img class="fotopeli" src="${linkimagen}${results.poster_path}" alt="" id="casapapel"></a>
+             </article>`
+        }
+
+
+        })
+    .catch(function(error){
+        console.log(`El error fue: ${error}`);
+    })
+
+});
