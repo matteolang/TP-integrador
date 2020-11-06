2d7a529521276243a0c3224772dc568978714252
Matteo Lang <matteolang@air-de-maru.lan>
2020-11-06 01:37:18 -0300

    Pagina Home casi terminada

--- js/index.js
+++ js/index.js
@@ -27,9 +27,6 @@ fetch(`https://api.themoviedb.org/3/movie/popular?api_key=c5fa76b40f5a5ea03c6014
             </a>
             </li>
              `
-           // `<article class="caja">
-            //<a href="serieDetail.html" class="pelicula-serie"><img class="fotopeli" src="${linkimagen}${results.poster_path}" alt="" id="casapapel"></a>
-             //</article>`
         }
 
 
@@ -38,8 +35,62 @@ fetch(`https://api.themoviedb.org/3/movie/popular?api_key=c5fa76b40f5a5ea03c6014
         console.log(`El error fue: ${error}`);
     })
 
+    var imagenesContenedorNuevas = document.querySelector(`#container-nuevas`)
+
+    fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=c5fa76b40f5a5ea03c60140eade37d35&language=en-US&page=1`)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+    
+            
+            for (let index = 0; index < data.results.length; index++) {
+                var resultss = data.results[index]
+            
+                imagenesContenedorNuevas.innerHTML += 
+                `
+            <li class="casapapel">
+            <a href="movieDetail.html">
+            <img src="${linkimagen}${resultss.poster_path}" alt="">
+            </a>
+            </li>
+             `
+    
+            }
+            })
+        .catch(function(error){
+            console.log(`El error fue: ${error}`);
+        })
+
+        var imagenesContenedorMejoresPuntuadas = document.querySelector(`#container-mejores-puntuadas`)
+
+        fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=c5fa76b40f5a5ea03c60140eade37d35&language=en-US&page=1`)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(data){
+                console.log(data);
+        
+                
+                for (let index = 0; index < data.results.length; index++) {
+                    var resultsss = data.results[index]
+                
+                    imagenesContenedorMejoresPuntuadas.innerHTML += 
+                    `
+                <li class="casapapel">
+                <a href="movieDetail.html">
+                <img src="${linkimagen}${resultsss.poster_path}" alt="">
+                </a>
+                </li>
+                 `
+        
+                }
+                })
+            .catch(function(error){
+                console.log(`El error fue: ${error}`);
+            })
+
+
 });
 
-//`<article class="caja">
-  //          <a href="serieDetail.html" class="pelicula-serie"><img class="fotopeli" src="${linkimagen}${results.poster_path}" alt="" id="casapapel"></a>
-    //         </article>`
