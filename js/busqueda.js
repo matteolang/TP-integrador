1817356663abb21af90ca09fd3abf4e74944d219
Matteo Lang <matteolang@air-de-maru.lan>
2020-11-23 16:23:07 -0300

    empeze resultados de busqueda

--- js/busqueda.js
+++ js/busqueda.js
@@ -1,8 +1,32 @@
 window.addEventListener(`load`, function() {
     
 
+    var sectionResultadosPelis = document.querySelector(`.resultados-peliculas`)
+    var sectionResultadosSeries = document.querySelector(`.resultados-series`)
 
+    var queryString = location.search
+    var queryStringObj = new URLSearchParams (queryString)
+    var queryStringObjBarraBusqueda = queryStringObj.get(`barrabusqueda`)
+    var queryStringObjTipo = queryStringObj.get(`tipo`)
 
+    console.log(queryStringObjBarraBusqueda)
+    console.log(queryStringObjTipo)
+
+    
+
+//????????????
+    if (queryStringObjTipo == `peliculas`) {
+        
+        sectionResultadosPelis.innerHTML += 
+        `${queryStringObjBarraBusqueda}`
+
+    }
+    else if (queryStringObjTipo == `series`){
+
+        sectionResultadosSeries.innerHTML += 
+        `${queryStringObjBarraBusqueda}`
+
+    }
   
 
 
